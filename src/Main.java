c328f4942025f0dba11240b6279e1d45b743ee14
Hyunjae <guswoqkrtk@gmail.com>
2016-12-21 02:39:47 +0900

    Add Bot2

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
+        /*
         for(Pack pack : Nipponsei.all()) {
             System.out.printf("pack: %s filename: %s size: %sM\n", pack.getPackNumber(), pack.getFileName(), pack.getFileSize());
         }
+        */
 
-        Bot bot = new Bot();
-
-        while(!bot.isJoined()) {
-            Thread.sleep(100); // Wait for the bot to join
-        }
+        Bot2 bot = new Bot2("irc.rizon.net", "bot", "#nipponsei");
 
         Scanner scanner = new Scanner(System.in);
         while(true) {
             String s = scanner.nextLine();
             bot.sendXdcc("Nippon|zongzing", s);
         }
-
     }
 }
